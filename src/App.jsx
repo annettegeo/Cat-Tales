1cfb8978a01e7a86b9e83ed8fdb7300910d5adec
Annette George Padippura <147926113+annettegeo@users.noreply.github.com>
2024-11-03 12:10:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ import audioFile from './assets/meow.mp3';
 
 const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19];
 
-//Returns timestamps for each word in the audio when the button is clicked
+//Return timestamps for each word in the audio when the button is clicked
 const getWordTimestamps = () => {
   return [
     { start: 0.05, end: 1.00 },
